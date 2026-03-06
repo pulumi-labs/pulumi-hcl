72eea6072252f720857b6b6d85269a626542b944
iwahbe <me@iwahbe.com>
2026-03-06 23:38:23 +0000

    Fix `l2-keywords`
    The converter already supported resource blocks, so this test passes by
    simply removing it from expectedEjectFailures and accepting the snapshots.

---
+++ cmd/pulumi-language-hcl/testdata/eject-pcl/l2-keywords/main.pp
@@ -0,0 +1,10 @@
+resource "firstResource" "keywords:index:SomeResource" {
+  builtins = "builtins"
+  property = "property"
+}
+
+resource "secondResource" "keywords:index:SomeResource" {
+  builtins = firstResource.builtins
+  property = firstResource.property
+}
+
