df03073afddea79768ed1b849c3e76e0ec4ac11f
iwahbe <me@iwahbe.com>
2026-03-09 15:37:20 +0000

    Add `l2-resource-option-protect`
    The converter already handles lifecycle { prevent_destroy } → protect
    conversion, so this test passes without any code changes.

---
+++ cmd/pulumi-language-hcl/testdata/eject-pcl/l2-resource-option-protect/main.pp
@@ -0,0 +1,18 @@
+resource "protected" "simple:index:Resource" {
+  value = true
+  options {
+    protect = true
+  }
+}
+
+resource "unprotected" "simple:index:Resource" {
+  value = true
+  options {
+    protect = false
+  }
+}
+
+resource "defaulted" "simple:index:Resource" {
+  value = true
+}
+
