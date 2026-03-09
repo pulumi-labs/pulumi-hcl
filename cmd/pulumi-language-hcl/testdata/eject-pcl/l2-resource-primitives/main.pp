b903015ec0b199068eadcbab11d9dfa4f7cc247b
iwahbe <me@iwahbe.com>
2026-03-09 16:26:47 +0000

    Add `l2-resource-primitives`
    Remove l2-resource-primitives from expectedEjectFailures. The converter
    already supports resource blocks, so both the run and eject tests pass
    without any code changes.

---
+++ cmd/pulumi-language-hcl/testdata/eject-pcl/l2-resource-primitives/main.pp
@@ -0,0 +1,12 @@
+resource "res" "primitive:index:Resource" {
+  boolean     = true
+  float       = 3.14
+  integer     = 42
+  string      = "hello"
+  numberArray = [-1, 0, 1]
+  booleanMap = {
+    "t" = true
+    "f" = false
+  }
+}
+
