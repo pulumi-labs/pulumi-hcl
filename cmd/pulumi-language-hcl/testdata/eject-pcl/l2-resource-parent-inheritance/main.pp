1012553b66f59d83079fd766a90f16aed988f2d8
iwahbe <me@iwahbe.com>
2026-03-09 16:23:58 +0000

    Add `l2-resource-parent-inheritance`
    Remove l2-resource-parent-inheritance from expectedEjectFailures.
    The converter already supports the constructs used by this test.

---
+++ cmd/pulumi-language-hcl/testdata/eject-pcl/l2-resource-parent-inheritance/main.pp
@@ -0,0 +1,49 @@
+resource "provider" "pulumi:providers:simple" {
+}
+
+resource "parent1" "simple:index:Resource" {
+  value = true
+  options {
+    provider = provider
+  }
+}
+
+resource "child1" "simple:index:Resource" {
+  value = true
+  options {
+    parent = parent1
+  }
+}
+
+resource "orphan1" "simple:index:Resource" {
+  value = true
+}
+
+resource "parent2" "simple:index:Resource" {
+  value = true
+  options {
+    retainOnDelete = true
+    protect        = true
+  }
+}
+
+resource "child2" "simple:index:Resource" {
+  value = true
+  options {
+    parent = parent2
+  }
+}
+
+resource "child3" "simple:index:Resource" {
+  value = true
+  options {
+    parent         = parent2
+    retainOnDelete = false
+    protect        = false
+  }
+}
+
+resource "orphan2" "simple:index:Resource" {
+  value = true
+}
+
