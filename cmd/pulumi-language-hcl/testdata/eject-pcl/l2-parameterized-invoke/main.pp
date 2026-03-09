c1396857f585be16346579d1a42a74233798284a
iwahbe <me@iwahbe.com>
2026-03-09 10:37:57 +0000

    Add `l2-parameterized-invoke`
    The converter already supported data blocks and parameterized package
    resolution, so this test passes without any code changes. Removed from
    expectedEjectFailures and generated the snapshot files.

---
+++ cmd/pulumi-language-hcl/testdata/eject-pcl/l2-parameterized-invoke/main.pp
@@ -0,0 +1,16 @@
+package "subpackage" {
+  baseProviderName    = "parameterized"
+  baseProviderVersion = "1.2.3"
+  parameterization {
+    name    = "subpackage"
+    version = "2.0.0"
+    value   = "SGVsbG9Xb3JsZA=="
+  }
+}
+
+output "parameterValue" {
+  value = invoke("subpackage:index:doHelloWorld", {
+    input = "goodbye"
+  }).output
+}
+
