d108b6cf5f2576214c2b03a1212ba04422cdabbd
iwahbe <me@iwahbe.com>
2026-03-09 10:27:53 +0000

    Add `l2-namespaced-provider`
    The converter already supports resource blocks with schema-based token
    resolution, so this test passes without any code changes. Remove it from
    expectedEjectFailures and add the generated snapshot files.

---
+++ cmd/pulumi-language-hcl/testdata/eject-pcl/l2-namespaced-provider/main.pp
@@ -0,0 +1,9 @@
+resource "componentRes" "component:index:ComponentCustomRefOutput" {
+  value = "foo-bar-baz"
+}
+
+resource "res" "namespaced:index:Resource" {
+  value       = true
+  resourceRef = componentRes.ref
+}
+
