763264a2436a6f40fadabd1a15ae8d21c9a196be
iwahbe <me@iwahbe.com>
2026-03-09 14:11:00 +0000

    Add `l2-resource-names`
    Enable the l2-resource-names eject test. The converter already supports
    resource blocks, so this test passes without any code changes.

---
+++ cmd/pulumi-language-hcl/testdata/eject-pcl/l2-resource-names/main.pp
@@ -0,0 +1,24 @@
+resource "res1" "names:index:ResMap" {
+  value = true
+}
+
+resource "res2" "names:index:ResArray" {
+  value = true
+}
+
+resource "res3" "names:index:ResList" {
+  value = true
+}
+
+resource "res4" "names:index:ResResource" {
+  value = true
+}
+
+resource "res5" "names:mod:Res" {
+  value = true
+}
+
+resource "res6" "names:mod/nested:Res" {
+  value = true
+}
+
