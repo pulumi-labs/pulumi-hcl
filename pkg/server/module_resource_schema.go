7fdb8907376a8551216b8ce2a11d6e9aa67b36a9
Ian Wahbe <me@iwahbe.com>
2026-07-02 10:23:28 +0200

    Publish to NuGet (#311)

--- pkg/server/module_resource_schema.go
+++ pkg/server/module_resource_schema.go
@@ -76,6 +76,7 @@ func moduleResourceSchema(version string) pulumischema.PackageSpec {
 				`{"importBasePath":"github.com/pulumi-labs/pulumi-hcl/sdk/go/hcl","respectSchemaVersion":true}`),
 			"nodejs": pulumischema.RawMessage(`{"packageName":"@pulumi-labs/hcl"}`),
 			"python": pulumischema.RawMessage(`{"packageName":"pulumi_labs_hcl"}`),
+			"csharp": pulumischema.RawMessage(`{"rootNamespace":"Pulumi.Labs"}`),
 		},
 		Resources: map[string]pulumischema.ResourceSpec{
 			moduleResourceToken: {
